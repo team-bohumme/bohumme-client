09605aae953f1160a4449d8caf950c270c65f6f9
jeonghoon11 <jih361@naver.com>
2025-06-28 02:24:09 +0900

    init: lint import 정렬 순서 절대경로 추가

--- config/eslint/.eslintrc.cjs
+++ config/eslint/.eslintrc.cjs
@@ -32,7 +32,11 @@ module.exports = {
           ['^node:'],
           ['^react$', '^@?\\w'],
           ['^@bofit/'],
-          //TODO add more specific imports ex)   ['^@shared/'], @shared/ 별칭으로 시작하는 모듈
+          ['^@app/'],
+          ['^@pages/'],
+          ['^@widgets/'],
+          ['^@shared/'],
+          ['^@features/'],
           ['^\\u0000'],
           ['^\\.'],
           ['^.+\\.css$'],
