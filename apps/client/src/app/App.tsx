f5f8b2057ad279f6792327f94a8560df54b69ab8
jogpfls <joerin4177@gmail.com>
2025-06-30 01:23:25 +0900

    init: QueryClientProvider 및 Devtools 설정

--- apps/client/src/app/App.tsx
+++ apps/client/src/app/App.tsx
@@ -1,13 +1,18 @@
 import { Button } from '@bds/ui';
+import { QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 import CommunityPage from '@pages/community/CommunityPage';
 
+import { queryClient } from '@shared/utils/query-client';
+
 function App() {
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <Button />
       <CommunityPage />
-    </>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   );
 }
 
