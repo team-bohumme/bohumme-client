d4b08462d3c9f7ba6c221d1baa1a6aef96a86df6
1jiwoo27 <chloey1227@naver.com>
2025-07-01 06:30:59 +0900

    feat: storybook 설치

--- packages/design-system/.eslintrc.cjs
+++ packages/design-system/.eslintrc.cjs
@@ -1,5 +1,5 @@
 module.exports = {
   env: { browser: true, es2020: true },
-  extends: ['../../config/eslint/.eslintrc.cjs'],
+  extends: ['../../config/eslint/.eslintrc.cjs', 'plugin:storybook/recommended'],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
 };
