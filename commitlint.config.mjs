fc3bfae3705816a2333f8bc955e5da31c4bcbc1a
jogpfls <joerin4177@gmail.com>
2025-07-02 02:21:46 +0900

    feat: commitlint 파일 설정

---
+++ commitlint.config.mjs
@@ -0,0 +1,38 @@
+export default {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      [
+        'feat',
+        'fix',
+        'bug',
+        'refactor',
+        'design',
+        'style',
+        'docs',
+        'test',
+        'settings',
+        'chore',
+        'init',
+        'rename',
+        'remove',
+        'build',
+        'deploy',
+        'perf',
+      ],
+    ],
+    'subject-empty': [2, 'never'],
+    'type-case': [2, 'always', 'lowercase'],
+    'type-empty': [2, 'never'],
+    'subject-case': [0],
+    'references-empty': [0],
+  },
+  parserPreset: {
+    parserOpts: {
+      headerPattern: /^([^:]+):\s{1,2}(.+)$/,
+      headerCorrespondence: ['type', 'subject'],
+    },
+  },
+};
