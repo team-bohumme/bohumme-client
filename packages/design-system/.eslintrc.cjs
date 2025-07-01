22dab0fc877ed372925a46d6e324d4e6cfa6406e
gwagjiug <kwjo0228@naver.com>
2025-07-01 23:00:19 +0900

    feat: Storybook 설정 및 구성 요소 정리

--- packages/design-system/.eslintrc.cjs
+++ packages/design-system/.eslintrc.cjs
@@ -1,5 +1,5 @@
 module.exports = {
   env: { browser: true, es2020: true },
-  extends: ['../../config/eslint/.eslintrc.cjs', 'plugin:storybook/recommended'],
+  extends: ['../../config/eslint/.eslintrc.cjs'],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
 };
--- packages/design-system/vite.config.ts
+++ packages/design-system/vite.config.ts
@@ -1,15 +1,7 @@
 import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
-import path from 'node:path';
-import { fileURLToPath } from 'node:url';
-import { storybookTest } from '@storybook/addon-vitest/vitest-plugin';
-const dirname =
-  typeof __dirname !== 'undefined'
-    ? __dirname
-    : path.dirname(fileURLToPath(import.meta.url));
 
-// More info at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon
 export default defineConfig({
   plugins: [react(), vanillaExtractPlugin()],
   build: {
