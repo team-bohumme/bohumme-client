5b178c9621e1a8567ba82e37f049d3b13e87d72d
jogpfls <joerin4177@gmail.com>
2025-07-01 21:15:47 +0900

    feat: home-page 라우터 설정

--- apps/client/src/shared/router/router.tsx
+++ apps/client/src/shared/router/router.tsx
@@ -2,10 +2,12 @@ import { createBrowserRouter } from 'react-router';
 
 import GlobalLayout from './global-layout';
 import { routePath } from './path';
+import { globalRoutes } from './routes/global-routes';
 
 export const router = createBrowserRouter([
   {
     path: routePath.LAYOUT,
     element: <GlobalLayout />,
+    children: [...globalRoutes],
   },
 ]);
