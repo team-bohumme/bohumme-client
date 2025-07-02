7821970af27aefe0033c0de96c7eba37917e5385
gwagjiug <kwjo0228@naver.com>
2025-07-03 00:13:00 +0900

    chore: App 컴포넌트에서 Icon 제거

--- apps/client/src/app/App.tsx
+++ apps/client/src/app/App.tsx
@@ -2,7 +2,6 @@ import { ThemeProvider } from '@bds/ui';
 import { rootStyle } from '@bds/ui/styles';
 import { QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-import { Icon } from '@bds/icon';
 import { RouterProvider } from 'react-router';
 
 import { router } from '@shared/router/router';
@@ -14,7 +13,6 @@ function App() {
       <ThemeProvider className={rootStyle}>
         <RouterProvider router={router} />
         <ReactQueryDevtools initialIsOpen={false} />
-        <Icon name="ai" width={60} height={60} color="primary600" />
       </ThemeProvider>
     </QueryClientProvider>
   );
