5c20f33546cf0afc0f598e1da28bbcae0478e04b
jeonghoon11 <jih361@naver.com>
2025-07-05 22:34:30 +0900

    feat: storybook color적용

--- packages/bds-ui/.storybook/preview.tsx
+++ packages/bds-ui/.storybook/preview.tsx
@@ -1,5 +1,6 @@
 import type { Preview } from '@storybook/react';
 import '../src/styles/reset.css';
+import { themeClass } from '../src/styles/theme.css';
 
 const preview: Preview = {
   parameters: {
@@ -9,7 +10,18 @@ const preview: Preview = {
         date: /Date$/i,
       },
     },
+    a11y: {
+      element: '#root',
+      manual: false,
+    },
   },
+  decorators: [
+    (Story) => (
+      <div className={themeClass}>
+        <Story />
+      </div>
+    ),
+  ],
 };
 
 export default preview;
