17d95eb3843da5cdd2692095cf66001b0327786f
jogpfls <joerin4177@gmail.com>
2025-07-05 22:35:44 +0900

    chore: 테스트 삭제

--- apps/client/src/pages/home/home-page.tsx
+++ apps/client/src/pages/home/home-page.tsx
@@ -1,31 +1,11 @@
-import { Tab, TabProvider } from '@bds/ui';
-
-const TABS = [
-  { id: 1, value: '큰 병' },
-  { id: 2, value: '수술' },
-  { id: 3, value: '입원' },
-  { id: 4, value: '장해' },
-  { id: 5, value: '사망' },
-];
+import { Icon } from '@bds/icon';
 
 const HomePage = () => {
   return (
-    <>
-      <TabProvider initialValue={TABS[0].value}>
-        <Tab>
-          <Tab.List>
-            {TABS.map(({ id, value }) => (
-              <Tab.Item key={id} value={value} />
-            ))}
-          </Tab.List>
-        </Tab>
-        {TABS.map(({ id, value }) => (
-          <Tab.Panel tab={value} key={id}>
-            {value} 입니다.
-          </Tab.Panel>
-        ))}
-      </TabProvider>
-    </>
+    <div>
+      home-page 배포 테스트입니다 하룰라라
+      <Icon name="add" />
+    </div>
   );
 };
 
