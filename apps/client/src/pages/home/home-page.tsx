52a8da2bcfe2cc78c04da53494beff9da807f5de
jogpfls <joerin4177@gmail.com>
2025-07-05 22:35:19 +0900

    chore: Tab 컴포넌트 구현 테스트

--- apps/client/src/pages/home/home-page.tsx
+++ apps/client/src/pages/home/home-page.tsx
@@ -1,11 +1,31 @@
-import { Icon } from '@bds/icon';
+import { Tab, TabProvider } from '@bds/ui';
+
+const TABS = [
+  { id: 1, value: '큰 병' },
+  { id: 2, value: '수술' },
+  { id: 3, value: '입원' },
+  { id: 4, value: '장해' },
+  { id: 5, value: '사망' },
+];
 
 const HomePage = () => {
   return (
-    <div>
-      home-page 배포 테스트입니다 하룰라라
-      <Icon name="add" />
-    </div>
+    <>
+      <TabProvider initialValue={TABS[0].value}>
+        <Tab>
+          <Tab.List>
+            {TABS.map(({ id, value }) => (
+              <Tab.Item key={id} value={value} />
+            ))}
+          </Tab.List>
+        </Tab>
+        {TABS.map(({ id, value }) => (
+          <Tab.Panel tab={value} key={id}>
+            {value} 입니다.
+          </Tab.Panel>
+        ))}
+      </TabProvider>
+    </>
   );
 };
 
